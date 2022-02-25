8f869b1792f96081da93175888db7aedc24a99c0
symplyijay <ijeonyi@gmail.com>
2022-02-25 23:13:44 +0100

    added a lil functionality for subscribe input at the footer

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const subscribeInput = document.querySelector('#input');
+
 const subscribeForm = document.querySelector('.sub-form');
 const arrowImg = document.querySelector('#arrow-img');
 subscribeForm.addEventListener('keypress', function () {
