218e19b2cd96b835326b2935ec83290a1133c3d5
frugalcodes <samueligwe63@gmail.com>
2022-03-04 21:50:38 +0100

    did some edits on files

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const subscribeInput = document.querySelector('#input');
-const subscribeForm = document.querySelector('.sub-form');
-const arrowImg = document.querySelector('#arrow-img');
-subscribeForm.addEventListener('keypress', function () {
-	arrowImg.style.opacity = 0;
-});
